2117504e1f886fa96d1a920cda34ac580e8fc117
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2021-03-23 14:42:06 -0300

    dinamic loop

---
+++ src/components/FilmesListaIten.vue
@@ -0,0 +1,12 @@
+<template>
+    <li class="list-group-item">
+        <span>{{ filmeTitulo }}</span>
+        <button class="btn btn-success float-right">Selecionar</button>
+    </li>
+</template>
+
+<script>
+export default {
+    props: [ 'filmeTitulo' ]
+}
+</script>
